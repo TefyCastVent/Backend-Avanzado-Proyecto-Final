4ccadbe188df0b508a41662d637abdf56cafde8f
TefyCastVent <94312111+TefyCastVent@users.noreply.github.com>
2022-08-31 15:27:11 -0500

    levantamiento del servidor y conección a la base

---
+++ api/config/database.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import config  from "./index.js";
+
+const db = mongoose.connection
+
+db.on('connecting', () => {
+    console.log('Intentando conectar a la base de datos 🟨')
+})
+db.on('connected', () => {
+    console.log('Se ha conectado a la base de datos 🟩')
+})
+db.on('error', () => {
+    console.log('Error en la conexion de la base de datos 🟥')
+})
+export default () => {
+mongoose.connect(config.database.uri)};
